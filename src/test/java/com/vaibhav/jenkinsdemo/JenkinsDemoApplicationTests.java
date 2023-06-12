19f164b1f48d70b9d5b9894ef41946bdfbbef213
unknown <vaibhav8976kalgude@gmail.com>
2023-06-12 18:17:22 +0200

    first commit

---
+++ src/test/java/com/vaibhav/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,25 @@
+package com.vaibhav.jenkinsdemo;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing");
+		assertEquals(true,true);
+	}
+
+}
